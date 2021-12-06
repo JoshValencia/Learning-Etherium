5b80a686c2846c4961e1de8d37b408f543fda0db
"Jushua Valencia" <"jushuavalencia@gmail.com">
2021-12-06 21:09:39 +0800

    ICO finish

--- token/token.sol
+++ token/token.sol
@@ -84,3 +84,78 @@ contract Cryptos is ERC20Interface{
          return true;
      }
 }
+
+contract CryptosICO is Cryptos{
+    address public admin;
+    address payable public deposit;
+    uint tokenPrice = 10000000000000000 wei; 
+    //1 Cryptos = 0.01 BNB 1 BNB = 100 Cryptos
+    uint public hardCap = 300000000000000000000 wei; //300 BNB
+    uint public raisedAmount;
+    uint public saleStart = block.timestamp;
+    //if want to add 1 hour (block.timestamp + 3600 seconds [1 hour])
+    uint public saleEnd = block.timestamp + 604800;
+    //604800 seconds = 1 week
+    uint public tokenTradeStart = saleEnd + 604800;
+    //Tokens will be transferrable after 1 week (604800)
+    uint public maxInvestment = 2000000000000000000 wei; // 2 BNB
+    uint public minInvestment = 100000000000000000 wei; // 0.1 BNB
+
+    enum State {beforeStart, running, afterEnd, halted}
+    State public icoState;
+
+    constructor(address payable _deposit){
+        deposit = _deposit;
+        admin = msg.sender;
+        icoState = State.beforeStart;
+    }
+
+    modifier onlyAdmin(){
+        require(msg.sender == admin);
+        _;
+    }
+
+    function halt() public onlyAdmin{
+        icoState = State.halted;
+    }
+
+    function resume() public onlyAdmin{
+        icoState = State.running;
+    }
+
+    function changeDepositAddress(address payable newDeposit) public onlyAdmin{
+        deposit = newDeposit;
+    } 
+
+    function getCurentState() public view returns(State){
+        if(icoState == State.halted){
+            return State.halted;
+        }else if(block.timestamp < saleEnd){
+            return State.beforeStart;
+        }else if(block.timestamp >= saleStart && block.timestamp <= saleEnd){
+            return State.running;
+        }else{
+            return State.afterEnd;
+        }
+    }
+
+    event Invest(address investor, uint value, uint tokens);
+
+    function invest () payable public returns(bool){
+        require(icoState == State.running);
+
+        require(msg.value >= minInvestment && msg.value <= maxInvestment);
+        raisedAmount += msg.value;
+        require(raisedAmount <= hardCap);
+
+        uint tokens = msg.value / tokenPrice;
+
+        balances[msg.sender] += tokens;
+        balances[founder] -= tokens;
+        deposit.transfer(msg.value);
+
+        emit Invest(msg.sender, msg.value, tokens);
+
+        return true;
+    }
+}

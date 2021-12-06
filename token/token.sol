9d3a64bc147e2397593c18143a8a66c616327699
"Jushua Valencia" <"jushuavalencia@gmail.com">
2021-12-06 20:10:58 +0800

    finish token sol

--- token/token.sol
+++ token/token.sol
@@ -1,47 +1,86 @@
 //SPDX-License-Identifier: GPL-3.0
  
 pragma solidity >=0.5.0 <0.9.0;
-
+// ----------------------------------------------------------------------------
+// EIP-20: ERC-20 Token Standard
+// https://eips.ethereum.org/EIPS/eip-20
+// -----------------------------------------
+ 
 interface ERC20Interface {
-    function totalSupply() external view returns (uint256);
-    function balanceOf(address account) external view returns (uint256);
-    function transfer(address recipient, uint256 amount) external returns (bool);
-    // function allowance(address owner, address spender) external view returns (uint256);
-    // function approve(address spender, uint256 amount) external returns (bool);
-    // function transferFrom(
-    //     address sender,
-    //     address recipient,
-    //     uint256 amount
-    // ) external returns (bool);
-    event Transfer(address indexed from, address indexed to, uint256 value);
-    // event Approval(address indexed owner, address indexed spender, uint256 value);
+    function totalSupply() external view returns (uint);
+    function balanceOf(address tokenOwner) external view returns (uint balance);
+    function transfer(address to, uint tokens) external returns (bool success);
+    
+    function allowance(address tokenOwner, address spender) external view returns (uint remaining);
+    function approve(address spender, uint tokens) external returns (bool success);
+    function transferFrom(address from, address to, uint tokens) external returns (bool success);
+    
+    event Transfer(address indexed from, address indexed to, uint tokens);
+    event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
 }
-
+ 
+ 
 contract Cryptos is ERC20Interface{
     string public name = "Cryptos";
-    string public symbol = "CRYPT";
-    uint public decimal = 0;
+    string public symbol = "CRPT";
+    uint public decimals = 0; //18 is very common
     uint public override totalSupply;
-
+    
     address public founder;
     mapping(address => uint) public balances;
-
+    // balances[0x1111...] = 100;
+    
+    mapping(address => mapping(address => uint)) allowed;
+    // allowed[0x111][0x222] = 100;
+    
+    
     constructor(){
         totalSupply = 1000000;
         founder = msg.sender;
-        balances[msg.sender] = totalSupply;
+        balances[founder] = totalSupply;
     }
-
-    function balanceOf(address account) public view override returns (uint256){
-        return balances[account];
+    
+    
+    function balanceOf(address tokenOwner) public view override returns (uint balance){
+        return balances[tokenOwner];
     }
-
-    function transfer(address recipient, uint256 amount) public override returns (bool){
-        require(balances[msg.sender] >= amount, "Balance is too low!");
-        balances[recipient] += amount;
-        balances[msg.sender] -= amount;
-        emit Transfer(msg.sender, recipient, amount);
-
+    
+    
+    function transfer(address to, uint tokens) public override returns(bool success){
+        require(balances[msg.sender] >= tokens);
+        
+        balances[to] += tokens;
+        balances[msg.sender] -= tokens;
+        emit Transfer(msg.sender, to, tokens);
+        
         return true;
     }
+    
+    
+    function allowance(address tokenOwner, address spender) view public override returns(uint){
+        return allowed[tokenOwner][spender];
+    }
+    
+    
+    function approve(address spender, uint tokens) public override returns (bool success){
+        require(balances[msg.sender] >= tokens);
+        require(tokens > 0);
+        
+        allowed[msg.sender][spender] = tokens;
+        
+        emit Approval(msg.sender, spender, tokens);
+        return true;
+    }
+    
+    
+    function transferFrom(address from, address to, uint tokens) public override returns (bool success){
+         require(allowed[from][to] >= tokens);
+         require(balances[from] >= tokens);
+         
+         balances[from] -= tokens;
+         balances[to] += tokens;
+         allowed[from][to] -= tokens;
+         
+         return true;
+     }
 }
